c7534c77b0fa383237553117f0aabb22b3b33909
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-06-17 10:26:09 +0000

    re

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual puzzle, follow by a new line.
+ *
+ * return : Always 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
