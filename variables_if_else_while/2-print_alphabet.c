fbc6879b27977f72b0d56e7d809773a56c16042a
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-06-18 13:52:58 +0000

    re

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - change char value
+ *
+ * Return: Always 0
+ */
 int main(void)
 	{
 		char ch;
 
 		for (ch = 'a'; ch <= 'z'; ch++)
 			putchar(ch);
-	
+
 	putchar('\n');
 	return (0);
 	}
