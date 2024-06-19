3a938fb011b277921e46b8fea11cb7accc3b2f8b
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-06-19 09:10:41 +0000

    re

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prins = display all numbers base 10
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char c;
+
+		for (c = '0'; c <= '9'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
