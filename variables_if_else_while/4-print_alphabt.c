c5af712be987da34fcf6ff8c4935d2ad95f7bccf
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-06-19 09:49:56 +0000

    re

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,8 +10,8 @@ int main(void)
 {
 char ch;
 
-for (ch = 'a'; ch <= 'z'; ch++)
-	if (ch != 'e' && ch != 'q')
+	for (ch = 'a'; ch <= 'z'; ch++)
+		if (ch != 'e' && ch != 'q')
 		putchar(ch);
 
 	putchar('\n');
