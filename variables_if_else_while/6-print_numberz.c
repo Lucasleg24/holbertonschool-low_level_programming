562694e69ca52881db928e756a3574bfaebd50f3
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-06-19 09:17:00 +0000

    re

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - prins = display all numbers
+ *
+ * Return: alway 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
