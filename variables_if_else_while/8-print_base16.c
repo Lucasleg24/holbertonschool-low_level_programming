bcecfbbec7282b76d98b930621ead92d1288a1cf
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-06-19 11:31:45 +0000

    re

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,9 +9,13 @@
 int main(void)
 {
 	char c;
+	char h;
 
-	for (c = '0'; c <= 'F'; c++)
+	for (c = '0'; c <= '9'; c++)
 		putchar(c);
+
+	for (h = 'A'; h <= 'F'; h++)
+		putchar(h);
 
 	putchar('\n');
 	return (0);
