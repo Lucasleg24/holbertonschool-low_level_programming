6a1b5b9eaf289f47c9afe3872c2d97228c8e33ca
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-06-20 11:58:58 +0000

    re

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char A;
+
+	for (A = 'a'; A <= 'z'; A++)
+	{
+		_putchar(A);
+	}
+	_putchar('\n');
+
+}
