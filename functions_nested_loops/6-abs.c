3e19c8a08eed22b2396686092941134ac15cfcd0
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-06-21 08:04:28 +0000

    re

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_abs - comput absolu variable
+ * @v: variable
+ * Return: return absolut value
+ */
+
+int _abs(int v)
+{
+	if (v > '0')
+		return (v);
+	else
+		return (-v);
+}
