89dbf799a7016c295ff5b90c511a64ba37a30dd3
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-06-21 11:06:41 +0000

    re

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - add two integer
+ * @a: integer a
+ * @b: integer b
+ * Return: always 0
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	_putchar(sum);
+
+	return (0);
+}
