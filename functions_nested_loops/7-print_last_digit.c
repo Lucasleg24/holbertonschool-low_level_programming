d87a007225874d3015abae4b1de4875c76d8e471
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-06-21 08:16:45 +0000

    re

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print result modulo
+ * @a: variable
+ * Return: always 0
+ */
+
+int print_last_digit(int a)
+{
+	_putchar(a % 10);
+	_putchar('\n');
+	return (0);
+}
