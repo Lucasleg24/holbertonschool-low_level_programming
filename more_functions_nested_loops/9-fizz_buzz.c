03ed2287e0cc71977f842b01538a8dbba919c8f1
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-06-24 14:05:20 +0000

    re

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i, m3, m5;
+
+	for (i = 1; i <= 100; i++)
+	{
+		m3 = i % 3;
+		m5 = i % 5;
+
+		if (m3 == 0 && m5 == 0)
+			printf("FizzBuzz");
+
+		else if (m5 == 0)
+			printf("Buzz");
+
+		else if (m3 == 0)
+			printf("Fizz");
+
+		else
+			printf("%d", i);
+	
+	printf(" ");
+	}
+	putchar('\n');
+
+	return (0);
+}
