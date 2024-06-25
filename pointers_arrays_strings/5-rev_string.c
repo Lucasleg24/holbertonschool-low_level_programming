4a608a4c53beab2051cfd6c3040dbff540f2300e
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-06-25 13:50:05 +0000

    re

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string: reverse string
+ * @s: varaible
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int l;
+	int i;
+	char t;
+	int c = 0;
+
+	for (l = 0; s[l] != '\0'; l++)
+	{
+	}
+	c = l -1;
+	for (i = 0; i <= c; i++, c--)
+	{
+		t = s[i];
+		s[i] = s[c];
+		s[c] = t;
+	}
+}
