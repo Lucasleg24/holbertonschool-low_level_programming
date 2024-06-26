71f3a3f95af8e7e4410071eeb0fe3834c58d48a8
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-06-26 08:11:09 +0000

    re

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "2-strlen.c"
 
 /**
  * puts_half - print half of string
@@ -10,7 +11,7 @@ void puts_half(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (i < _strlen(str))
 	{
 		if (str[i] >= '5')
 		{
