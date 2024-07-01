d668622924ee082259cb47dc40f5ecd9203e3903
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-01 11:14:51 +0000

    re

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
- *
- *
+ * *_memcpy - change value
+ * @dest: array 2
+ * @src: array 1
+ * @n: variable 1
+ * Return: dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
@@ -10,4 +13,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
