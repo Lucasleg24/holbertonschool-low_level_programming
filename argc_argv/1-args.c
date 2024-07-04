4f2f736657b8c791ab71f21b062aa44b42f236d9
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-04 13:28:54 +0000

    re

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	for (count = 1; count < argc; count++)
+	{
+	}
+	printf("count\n");
+	return (0);
+}
+
