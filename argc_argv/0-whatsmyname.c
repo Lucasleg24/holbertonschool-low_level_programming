f248650bcb75b2cc2669d4c7e38541abd1d3b2b9
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-04 11:48:23 +0000

    re

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - fonction print name
+ * @argc: number argument
+ * @argv: array of argument
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+
+	printf("%s\n", argv[0]);
+	if (argc > 1)
+	{
+		for (count = 1; count < argc; count++)
+	{
+		printf("argv[%d] = %s\n", count, argv[count]);
+	}
+	}
+	else
+	{
+		printf("The command had no other arguments.\n");
+	}
+
+	return (0);
+}
