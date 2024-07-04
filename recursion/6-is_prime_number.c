601de76a37b173884f8efaad3f6f9beee08701a7
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-04 09:00:55 +0000

    re

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,11 +9,11 @@
 
 int increment(int n, int i)
 {
-	if (n % i != 0 && i == 9)
+	if (i * i > n)
 	{
 		return (1);
 	}
-	if (n % i == 0 && i != 1 && i != n)
+	if (n % i == 0)
 	{
 		return (0);
 	}
@@ -31,10 +31,10 @@ int increment(int n, int i)
 
 int is_prime_number(int n)
 {
-	if (n < 0)
+	if (n <= 1)
 	{
 		return (0);
 	}
 
-	return (increment(n, 0));
+	return (increment(n, 2));
 }
