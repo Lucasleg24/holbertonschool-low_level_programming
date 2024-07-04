b002d4fa8d9d9f13f1709fec51001e3c98e0041d
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-04 13:56:06 +0000

    re

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 
 /**
- *
- *
+ * main - count argc
+ * @argc: all parameter
+ * @argv: listing parameter
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
 	int count;
 
-	for (count = 1; count < argc; count++)
+	printf("%d\n", argc);
+	if (argc > 1)
+	{
+		for (count = 1; ount < argc; count++)
 	{
 	}
-	printf("count\n");
+	}
+	else
+	{
+		return (0);
+	}
+
 	return (0);
 }
-
