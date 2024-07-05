dc3d504c6c34872d4f8c728f2cd3a56addd2db32
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-05 14:24:18 +0000

    re

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -20,13 +20,13 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; j < argc; j++)
+		for (j = 0; argv[i][j]; j++)
 		{
-		if (!atoi(argv[j]))
-		{
-			printf("Error\n");
-			return (1);
-		}
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 		add = add + atoi(argv[i]);
 	}
