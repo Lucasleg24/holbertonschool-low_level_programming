32a087ec4e344a1a09ca8916c493eb274d4a5fef
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-05 13:00:41 +0000

    re

---
+++ argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function add arguments
+ * @argc: numbers arguments
+ * @argv: value arguments
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int add = 0;
+
+	if (argc < 1)
+	{
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		add = add + atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
