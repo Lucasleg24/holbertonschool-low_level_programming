cec1bed8e2a14170079f502e0904dbd902daf5df
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-10 11:10:19 +0000

    re

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -3,9 +3,26 @@
 #include <stdlib.h>
 
 /**
- *
- *
+ * *array_range - array of integer
+ * @min: value min
+ * @max: value max
+ * Return: 0 or NULL
  */
 
 int *array_range(int min, int max)
 {
+	int *t;
+	int i;
+
+	if (min > max)
+		return (NULL);
+	t = malloc(sizeof(int) * (max + 1));
+	if (t == 0)
+		return (NULL);
+	for (i = 0; i < (max + 1); i++)
+	{
+		t[i] = min;
+		min++;
+	}
+	return (t);
+}
