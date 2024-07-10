fd3b0907ecf89f96b3e1fae639c7453e9115ea22
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-10 12:27:37 +0000

    re

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,6 +13,8 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	printf("Name: %s\n", d->name);
+	if (d->age < 0)
+		printf("Age: (nil)\n");
 	printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
