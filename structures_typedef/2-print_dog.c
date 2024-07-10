d523cf8882de3462f29ff2ac3f093a4bb9297a52
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-10 12:25:41 +0000

    un vrai commit pour une fois

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,7 +10,11 @@ void print_dog(struct dog *d)
 {
 	if (d == 0)
 		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
 	printf("Owner: %s\n", d->owner);
 }
