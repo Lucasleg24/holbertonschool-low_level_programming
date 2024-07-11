44d9c44e67f20c5ea78e096ae5e87d5472b7f9a6
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-11 07:00:11 +0000

    re

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,6 +3,41 @@
 #include "1-strdup.c"
 
 /**
+ * *_strdup - copy string
+ * @str: string duplicate
+ * Return: 0 or NULL
+ */
+
+char *_strdup(char *str)
+{
+        char *t;
+        int i = 0;
+        int l;
+
+        if (str == NULL)
+        {
+                return (NULL);
+        }
+        for (l = 0; str[l] != '\0'; l++)
+        {
+        }
+        l++;
+
+        t = malloc(sizeof(char) * l);
+
+        if (t == 0)
+        {
+                return (NULL);
+        }
+        while (str[i] != '\0')
+        {
+                t[i] = str[i];
+                i++;
+        }
+        return (t);
+}
+
+/**
  * *new_dog - copy the dog
  * @name: name of the dog
  * @age: age of the dog
