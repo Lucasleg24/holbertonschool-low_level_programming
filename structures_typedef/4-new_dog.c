9dbe504f47469bf5f365345d4eb3eb2b46f45dad
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-11 11:39:38 +0000

    re

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,41 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
-#include "1-strdup.c"
-
-/**
- * *_strdup - copy string
- * @str: string duplicate
- * Return: 0 or NULL
- */
-
-char *_strdup(char *str)
-{
-        char *t;
-        int i = 0;
-        int l;
-
-        if (str == NULL)
-        {
-                return (NULL);
-        }
-        for (l = 0; str[l] != '\0'; l++)
-        {
-        }
-        l++;
-
-        t = malloc(sizeof(char) * l);
-
-        if (t == 0)
-        {
-                return (NULL);
-        }
-        while (str[i] != '\0')
-        {
-                t[i] = str[i];
-                i++;
-        }
-        return (t);
-}
+#include <stdio.h>
+char *_strdup(char *str);
 
 /**
  * *new_dog - copy the dog
@@ -49,11 +15,59 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
-	if (name == NULL || age == NULL || owner == NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == 0)
 		return (NULL);
-	_strdup(dog_t);
+	new_dog->name = _strdup(name);
+	if (name == 0)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->owner = _strdup(owner);
+	if (owner == 0)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->age = age;
 	return (new_dog);
+}
+
+/**
+ * _strdup - copy string
+ * @str: string duplicate
+ * Return: 0 or NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *t;
+	int i = 0;
+	int l;
+	
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (l = 0; str[l] != '\0'; l++)
+	{
+	}
+	l++;
+	
+	t = malloc(sizeof(char) * l);
+	
+	if (t == 0)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+		t[i] = str[i];
+		i++;
+	}
+	return (t);
 }
