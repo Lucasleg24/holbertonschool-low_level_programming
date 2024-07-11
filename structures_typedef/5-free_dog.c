24d2ea234a5fe23dd29bd3db0d82ca8af320fc7b
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-11 12:44:54 +0000

    re

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - liberer, delivrer
+ * @d: pointer
+ * Return: 0 or NULL
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	free(d);
+	return;
+}
