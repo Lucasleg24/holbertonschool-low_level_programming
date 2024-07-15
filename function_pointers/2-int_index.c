7d25af41bfe82414ec85ebfce204347a68bfb7c7
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-15 13:03:32 +0000

    re

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -17,6 +17,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 		return (-1);
 	if (array == 0)
 		return (-1);
+	if (cmp == 0)
+		return (-1);
 	while (i <= size)
 	{
 		result = cmp(*array);
