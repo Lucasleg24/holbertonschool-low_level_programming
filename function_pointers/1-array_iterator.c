886d4201c6734054f4e83808ed985783dfa7985c
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-15 09:49:47 +0000

    re

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - create array with function
+ * @array: array of value
+ * @size: size of array
+ * @action: function
+ * Return: always void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i = 0;
+
+	if (size <= 0)
+		return;
+	if (array == 0)
+		return;
+	if (action == 0)
+		return;
+
+	while (i < size)
+	{
+		action(*array);
+		i++;
+		array++;
+	}
+}
