300b022ddd37a267245f4ebb21cefd09800f494b
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-18 13:03:10 +0000

    task 3 test print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,7 +1,49 @@
 #include "variadic_functions"
 
 /**
- *
+ * print_int - print an int
+ * Return: void
+ */
+
+void print_int(void)
+{
+	printf("%d");
+}
+
+/**
+ * print_char - print a char
+ * Return: void
+ */
+
+void print_char(void)
+{
+	printf("%c");
+}
+
+/**
+ *  print_string - print a string
+ *  Return: void
+ */
+
+void print_string(void)
+{
+	printf("%s");
+}
+
+/**
+ * print_float - print a float
+ * Return: void
+ */
+
+void print_float(void)
+{
+	printf("%f");
+}
+
+/**
+ * print_all - print the result
+ * @format: check the format
+ * Return: return void
  */
 
 void print_all(const char * const format, ...)
@@ -10,9 +52,8 @@ void print_all(const char * const format, ...)
 	int i;
 	float f;
 	char *s;
-	char *p;
+	char *p = 0;
 
 	while (format[p] != NULL)
 	{
-		if (format[p] == c || format[p] == i || format[p] == f || format[p] == s)
-
+		if (format[p] == 'c' || format[p] == 'i' || format[p] == 'f' || format[p] == 's')
