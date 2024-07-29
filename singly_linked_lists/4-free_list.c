928c563ee9d39c7b4505eb56641367431ac004f0
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-29 13:16:21 +0000

    file t4

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -10,10 +10,14 @@ void free_list(list_t *head)
 {
 	list_t *t;
 
-	while (t != NULL)
+	while (head != NULL)
 	{
 		t = head->next;
-		free(head->str);
+
+		if (head->str != NULL)
+		{
+			free(head->str);
+		}
 		free(head);
 		head = t;
 	}
