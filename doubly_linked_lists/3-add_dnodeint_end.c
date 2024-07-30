2c64294a40067b075dadd6e041788a9d469fe4b2
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-30 09:01:57 +0000

    add test t3

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add node at end
+ * @head: header of list
+ * @n: variable
+ * Return: return the list
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *end;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	if (*head == NULL)
+	{
+		new->n = n;
+		new->prev = NULL;
+		new->next = NULL;
+		*head = new;
+		return (new);
+	}
+	else
+	{
+		end = *head;
+		while (end->next != NULL)
+		{
+			end = end->next;
+		}
+		end->next = new;
+		new->n = n;
+	}
+
+	return (new);
+}
