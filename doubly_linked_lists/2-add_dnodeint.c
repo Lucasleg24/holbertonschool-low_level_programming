55aac3dbb27395dd482db1c14c38ba2742904db9
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-30 08:22:41 +0000

    add resolve error t2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,6 +11,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
+	if (head == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
@@ -18,7 +21,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		new->n = n;
 		new->prev = NULL;
