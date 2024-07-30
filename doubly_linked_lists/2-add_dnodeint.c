50101123e099e199538ba2976ee82dfa329317c5
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-30 07:53:57 +0000

    add test error check t2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,14 +11,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	if (head == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
 	{
 		return (NULL);
+	}
+	if (head == NULL)
+	{
+		*head = new;
+		return (new);
 	}
 
 	new->n = n;
