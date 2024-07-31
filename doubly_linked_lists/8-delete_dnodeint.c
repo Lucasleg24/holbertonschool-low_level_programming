970394a8b65c549b90e1834ecb13ce7368a0518c
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-31 09:00:27 +0000

    add file t8

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - free all nodes
+ * @head: node value
+ * @index: position of index at free
+ * Return: 1 or -1
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+	unsigned int i;
+
+	if (head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = temp->next;
+		if (temp->next != NULL)
+			temp->next->prev = NULL;
+	}
+
+	for (i = 0; temp != NULL && i < index; i++)
+	{
+		temp = temp->next;
+	}
+
+	if (temp == NULL)
+		return (-1);
+	if (temp->next != NULL)
+	{
+		temp->next->prev = temp->prev;
+	}
+	if (temp->prev != NULL)
+	{
+		temp->prev->next = temp->next;
+	}
+	free(temp);
+	return (1);
+}
