3b38bc96a9fda0a53fb64c6250de76a539ecc9c2
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-31 12:35:51 +0000

    add t0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,6 +11,9 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int count;
 	unsigned int result = 0;
 
+	if (b == NULL)
+		return (0);
+
 	for (count = 0; b[count] != '\0'; count++)
 	{
 		if (b[count] < '0' || b[count] > '1')
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,5 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+#include <stdio.h>
+#include <stddef.h>
 
 unsigned int binary_to_uint(const char *b);
 
