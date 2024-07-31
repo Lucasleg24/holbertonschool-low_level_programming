14239b749ed871c7367e1d181fdd735db8e93d83
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-31 14:04:40 +0000

    test t1 p2

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -12,16 +12,27 @@ void print_binary(unsigned long int n)
 	unsigned long int result = 0;
 	unsigned long int v = n;
 
-	while (v != 0)
+	if (v == 0)
+		_putchar('0');
+	if (v == 1)
+		_putchar('1');
+
+	while (v > 1)
 	{
 		count = 1;
-		while (count * 2 <= v)
+		while (count * 2 < v)
 		{
 			count = count * 2;
 		}
 		result = result + count;
-		if (result <= n)
+		if (result <= n && result * 2 != n)
 			_putchar('1');
+		else if (result * 2 == n)
+		{
+			_putchar('1');
+			result = result * 2;
+			count = count * 2;
+		}
 		else
 		{
 			result = result - count;
