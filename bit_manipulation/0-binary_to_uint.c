4827ba7e7c2422df30a7513ef1732323d837b5d8
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-07-31 12:33:40 +0000

    add t0

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert binary number
+ * @b: string with value binary
+ * Return: return the binary number in decimal number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int count;
+	unsigned int result = 0;
+
+	for (count = 0; b[count] != '\0'; count++)
+	{
+		if (b[count] < '0' || b[count] > '1')
+			return (0);
+
+		result = result * 2 + (b[count] - '0');
+	}
+	return (result);
+}
