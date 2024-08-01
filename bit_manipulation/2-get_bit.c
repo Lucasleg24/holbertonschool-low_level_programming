9ec645fd01abcba0a37c82d790e7eba10b577924
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-08-01 11:12:21 +0000

    add t2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,9 +1,21 @@
 #include "main.h"
 
 /**
- *
+ * get_bit - function return bit value
+ * @n: variable number
+ * @index: value of placement in n
+ * Return: return value of bit
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int bi;
 
+	bi = sizeof(n) * 8 - 1;
+
+	if (index > bi)
+		return (-1);
+
+
+	return (n >> index & 1);
+}
