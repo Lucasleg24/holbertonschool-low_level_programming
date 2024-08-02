85e84460ff29582b295d9746c9c920eefc361698
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-08-02 13:32:35 +0000

    re

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
 	if (from == -1)
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 
-	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	to = open(argv[2],O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (to == -1)
 		error_exit(99, "Error: Can't write to %s\n", argv[2]);
 
