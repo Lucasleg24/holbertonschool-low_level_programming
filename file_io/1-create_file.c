b8058ccdff7309e1c2a503d091f8648756178076
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-08-02 09:57:46 +0000

    add test t1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	int lenght = 0;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 	id = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (id == -1)
 		return (-1);
