b9564f06334c7e913e597fb78612c69eb9e1a180
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-08-02 09:56:15 +0000

    add test t1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -4,7 +4,7 @@
  * create_file - create a new file
  * @filename: name of a new file
  * @text_content: content of a new file
- * Return: return value of lenght
+ * Return: -1 if fail or 1 is success
  */
 
 int create_file(const char *filename, char *text_content)
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (0);
-	id = open(filename, O_RDWR | O_TRUNC, 0600);
+	id = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (id == -1)
 		return (-1);
 	if (text_content == NULL)
